78aebbe28be991fc839186eccdb1bc8a0e1d017d
David Lyon <david.lyon@hackerpads.com>
2013-04-09 16:42:34 +1000

    Initialisation

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 clixx.io
 ========
 
-Clixx.io, cross platform peripherals for Internet Appliances
+Clixx.io is cross platform peripherals for Internet Appliances.
+
+It's a collection of devices on a network that talk to each other
+with JSON. 
+
+For example, Switches, LEDs and binary devices exchange information
+with the 'level' value. Which can be either 'High' or 'Low'.
+
+ADCs, DACs exchange information with 'value' parameter.
+
+There's a Web Interface also under construction making all the
+devices able to be driven from the Browser.
---
+++ setup.py
@@ -0,0 +1,32 @@
+import sys
+if sys.version_info < (2, 5):
+    print(sys.stderr, "{}: need Python 2.5 or later.".format(sys.argv[0]))
+    print(sys.stderror, "Your python is {}".format(sys.version))
+    sys.exit(1)
+
+from setuptools import setup
+
+setup(
+    name = "clixx-io",
+    version = "0.0.2",
+    url = "http://github.com/clixx-io/clixx.io",
+    license = "BSD",
+    description = "A python library for controlling clixx.io peripherals",
+    author = "David Lyon",
+    author_email = "david.lyon@hackerpads.com",
+    package_dir = {'': 'src'},
+    packages = [''],
+# use_2to3 = True,
+    test_suite = "tests.tests",
+    install_requires = ['setuptools'],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: System :: Logging',
+    ]
+)
