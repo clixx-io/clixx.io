a5ac3f1aaa0d42f39bf16e3bc150e0047c8d27e2
clixx-io <david.lyon@hackerpads.com>
2014-01-21 23:49:04 +1100

    Updated event framework

---
+++ eventframework/loop.cpp
@@ -0,0 +1,40 @@
+
+/* Simple Loop
+
+A trivial simple example of using circuits and timers.
+*/
+
+#include <stdio.h>
+#include "clixxIO.hpp"
+
+class App : public clixxIOApp{
+
+    int loopevent(){
+        /* hello loop handler
+
+         This runs repeatedly
+        */
+
+        printf("Program is running");
+    };
+    
+    int started(){
+        /* started Event handler
+
+        Setup a loop event to continuously run. This would
+        be like the for(;;) loop in a traditional embedded
+        program
+        
+        */
+
+        addLoopEvent(&loopevent());
+    };
+    
+};
+
+// Final statement should be => App().run()
+
+main(){
+  App m;
+  m.run();
+}
