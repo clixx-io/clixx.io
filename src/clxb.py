e3e402a5cb290d1f2b4603d7f38a67b40483e768
David Lyon <david.lyon@hackerpads.com>
2014-01-30 14:58:32 +1100

    Added builder script

---
+++ src/clxb.py
@@ -0,0 +1,18 @@
+# /usr/bin/python
+
+import optparse
+
+program_input = []
+
+def load_source(filepath):
+    """
+    Load the specified file into memory for various scans
+    """
+    program_input = [line.strip() for line in open('filename')]
+
+
+
+if __name__ == "__main__":
+
+    print "Event-framework program preprocessor"
+
