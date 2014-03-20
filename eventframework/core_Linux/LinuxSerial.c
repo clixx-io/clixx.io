2b2d0ec584f4401de1f57e38e4bf5adf94c9ce38
djlyon <david.lyon@hackerpads.com>
2014-03-20 12:03:30 +1100

    Initial checkin

--- eventframework/core_Linux/LinuxSerial.c
+++ eventframework/core_Linux/LinuxSerial.c
@@ -1,3 +1,30 @@
+/*
+ * Copyright (c) 2014 clixx.io
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *  * Neither the name of the Clixx.io nor the names of its contributors 
+ *    may be used to endorse or promote products derived from this software 
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL CLIXX.IO BE LIABLE FOR ANY DIRECT, INDIRECT, 
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES LOSS OF USE, DATA, 
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -18,11 +45,12 @@
 
 #include <getopt.h>
 
-#include "linuxSerial.h"
-
-#define GPS_BAUDRATE B4800
-
 /**********************************************************************/
+
+int serial_feed_setup(const char *portname, long baudrate);
+int serial_feed_capture(int tty_fd, char *buffer, int buffersize, int dumpchars = 0);
+int serial_feed_close(int tty_fd);
+int term_setup(int fd, long baudrate);
 
 #ifdef UUCP_LOCK_DIR
 
@@ -150,8 +178,7 @@ establish_child_signal_handlers (void)
 	sigaction (SIGTERM, &empty_action, NULL);
 }
 
-int
-term_setup(int fd)
+int term_setup(int fd, long baudrate)
 {
 	struct termios tio;
 	
@@ -162,15 +189,15 @@ term_setup(int fd)
 		return(r);
 	}
 	
-	r = cfsetospeed(&tio, GPS_BAUDRATE);
+	r = cfsetospeed(&tio, baudrate);
 	if ( r < 0 ) {
-		fatal("cannot Set serial port to baudrate %d: %s", GPS_BAUDRATE, strerror(errno));
+		fatal("cannot Set serial port to baudrate %d: %s", baudrate, strerror(errno));
 		return(r);
 	}
 			
-	r = cfsetispeed(&tio, GPS_BAUDRATE);
+	r = cfsetispeed(&tio, baudrate);
 	if ( r < 0 ) {
-		fatal("cannot Set serial port to baudrate %d: %s", GPS_BAUDRATE, strerror(errno));
+		fatal("cannot Set serial port to baudrate %d: %s", baudrate, strerror(errno));
 		return(r);
 	}
 
@@ -188,7 +215,7 @@ term_setup(int fd)
 
 struct pollfd fds[1];
 
-int serial_feed_setup(const char *portname)
+int serial_feed_setup(const char *portname, long baudrate)
 {
 	establish_child_signal_handlers();
 
@@ -202,7 +229,7 @@ int serial_feed_setup(const char *portname)
 	if (tty_fd < 0)
 		fatal("cannot open %s: %s", portname, strerror(errno));
 
-	int r = term_setup(tty_fd);
+	int r = term_setup(tty_fd, baudrate);
 	if ( r < 0 )
 		fatal("failed to setup serial port on device %s:", 
 			  portname);
