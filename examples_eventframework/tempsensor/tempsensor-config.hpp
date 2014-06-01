833a7d56abfca88ef1c20e3a468b9a1bed9fe33a
David Lyon <david.lyon@hackerpads.com>
2014-06-02 09:26:48 +1000

    Examples added

---
+++ examples_eventframework/tempsensor/tempsensor-config.hpp
@@ -0,0 +1,16 @@
+/* Configuration
+
+This file contains the pin settings for any devices and can
+safely be hand edited.
+
+*/
+
+/*
+ for x in device_list.keys():
+	device_list[x]
+	
+ The results should be something like:
+ #define Button1 D1_i
+ #define Led1 D2_o
+ 
+*/
