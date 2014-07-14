be7ffeb9ecd3a3a88447c708732831409ce14d04
clixx-io <david.lyon@hackerpads.com>
2014-07-14 16:37:16 +1000

    Added project paths

--- webinterface/web-clixx.io.py
+++ webinterface/web-clixx.io.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-""" Generate a home page with a list of projects
+    """ Generate a home page with a list of projects
     """
     plist = ""
     projects = clixxIOListProjects()
@@ -14,6 +14,19 @@ def hello_world():
         plist = plist + "<p>" + p + "</p>"
     
     return "<h1>Welcome to clixx.io</h1>" + plist
+
+@app.route('/iot/<projectname>')
+def show_project_profile(projectname):
+    # show the user profile for that user
+    projects = clixxIOListProjects()
+    if projectname in projects:
+        pc = open(clixxIOlProjectConfigFilename(projectname))
+        lines = pc.read()
+        pc.close()
+        return "<h1>%s</h1>" % projectname + "<p>" + lines + "</p>"
+        
+    else:
+        return 'Project %s is not a valid project.' % projectname
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
