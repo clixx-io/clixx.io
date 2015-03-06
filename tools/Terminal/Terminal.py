f2126dd5209675e89d2987f204bf5e84c7bf4aae
clixx-io <david.lyon@clixx.io>
2015-03-06 23:17:17 +1100

    Updated Serial port detection

--- tools/Terminal/Terminal.py
+++ tools/Terminal/Terminal.py
@@ -1,18 +1,38 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import os, sys
+import os, sys, platform
 import serial
 from PySide import QtGui, QtCore
 from PySide.QtCore import *
 from PySide.QtGui import *
-from portsListener import PortsListener
 from serialevents import SerialEvents
 from pyterm_exceptions import *
 import time
 
 verbose = 1
 
+# A function that tries to list serial ports on most common platforms
+def list_serial_ports():
+    system_name = platform.system()
+    if system_name == "Windows":
+        # Scan for available ports.
+        available = []
+        for i in range(256):
+            try:
+                s = serial.Serial(i)
+                available.append("COM%d" % (i+1))
+                s.close()
+            except serial.SerialException:
+                pass
+        return available
+    elif system_name == "Darwin":
+        # Mac
+        return glob.glob('/dev/tty*') + glob.glob('/dev/cu*')
+    else:
+        # Assume Linux or something else
+        return glob.glob('/dev/ttyS*') + glob.glob('/dev/ttyUSB*')
+        
 def main(args):
 	
 	def quit():
@@ -40,13 +60,12 @@ def main(args):
 	
 	a = QtGui.QApplication(args)
 	
-	#matchedPorts = PortsListener.getPorts()
-	matchedPorts = ["COM4:"]
+	matchedPorts = list_serial_ports()
 	
 	ports_cb = QtGui.QComboBox()
 	ports_cb.currentIndexChanged[str].connect(portChanged)
 	
-	for port in matchedPorts:
+	for port in reversed(matchedPorts):
 		ports_cb.addItem(port)	# TODO: add QVariant with port data
 	
 	connect = QPushButton("Connect")
@@ -66,11 +85,6 @@ def main(args):
 	se = SerialEvents(s)
 	se.readyRead.connect(write)
 	se.start()
-	
-	# try:
-	# 	s = serial.Serial('/dev/cu.usbserial-A6008cB6')
-	# except (FileNotFoundError, serial.SerialException):
-	# 	print("Error")
 	
 	window =  QWidget()
 	layout =  QVBoxLayout()
