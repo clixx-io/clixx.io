3aee566e9c7ec49d6dbd41c6da1ab33d6083fb02
clixx-io <david.lyon@clixx.io>
2015-03-19 00:35:32 +1100

    Added circuits dependency

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
 # use_2to3 = True,
     test_suite = "tests.tests",
     zip_safe = False,
-    install_requires = ['setuptools','mako','pyside','flask'],
+    install_requires = ['setuptools','mako','pyside','flask','circuits'],
     classifiers = [
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
