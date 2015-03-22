3b0f119b5f23b5e4bdf13713093fc91996a115d6
clixx-io <david.lyon@clixx.io>
2015-03-22 19:24:14 +1100

    Added Serial port detection

--- source/python/clixxIO.py
+++ source/python/clixxIO.py
@@ -952,3 +952,25 @@ def clixxIOInfo(deviceId):
     Read known summary metadata for a device
     """
     return results
+
+# A function that tries to list serial ports on most common platforms
+def list_serial_ports():
+    system_name = platform.system()
+    if system_name == "Windows":
+        # Scan for available ports.
+        available = []
+        for i in range(256):
+            try:
+                s = serial.Serial(i)
+                available.append("COM%d" % (i+1))
+                s.close()
+            except serial.SerialException:
+                pass
+        return available
+    elif system_name == "Darwin":
+        # Mac
+        return glob.glob('/dev/tty*') + glob.glob('/dev/cu*')
+    else:
+        # Assume Linux or something else
+        return glob.glob('/dev/ttyS*') + glob.glob('/dev/ttyUSB*')
+        
