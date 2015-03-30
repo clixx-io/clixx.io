667d77a0c4d0576c407a59a35e0e24466c10d66a
clixx-io <david.lyon@hackerpads.com>
2015-03-30 16:36:08 +1100

    Updated for path changes

--- examples_linux/iot-subscribe/compile.sh
+++ examples_linux/iot-subscribe/compile.sh
@@ -1,2 +1,2 @@
 # ToDo: Replace with a Makefile
-g++ -D TARGET_LINUX -Wall -o iot-subscribe iot-subscribe.cpp -I ../../iot-framework/ -L ../../iot-framework -l clixxIO_Linux -l mosquittopp
+g++ -D USE_MOSQUITTO -D TARGET_LINUX -Wall -o iot-subscribe iot-subscribe.cpp -I ../../source/iot-framework/ -L ../../source/iot-framework -l clixxIO_Linux -l mosquittopp
