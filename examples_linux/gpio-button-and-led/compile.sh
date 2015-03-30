f944d574a61fa72a7f9e99251120c5a2b793ee8c
clixx-io <david.lyon@hackerpads.com>
2015-03-30 16:55:28 +1100

    Updated compilation problem

--- examples_linux/gpio-button-and-led/compile.sh
+++ examples_linux/gpio-button-and-led/compile.sh
@@ -1,2 +1,2 @@
 # ToDo: Replace with a Makefile
-g++ -Wall -g -D TARGET_LINUX -Wall -o gpio-test gpio-test.cpp -I ../../iot-framework/ -L ../../iot-framework -l clixxIO_Linux -l mosquittopp
+g++ -Wall -g -D TARGET_LINUX -Wall -o gpio-test gpio-test.cpp -I ../../source/iot-framework/ -L ../../source/iot-framework -l clixxIO_Linux -l mosquittopp
