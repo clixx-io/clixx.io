d565357026cb5df6f10a66fb5b4fdf7579794dc8
clixx-io <david.lyon@clixx.io>
2015-06-18 13:05:42 +1000

    Fixed counter algorithm

--- source/tools/logline-counter/logline-counter.py
+++ source/tools/logline-counter/logline-counter.py
@@ -6,6 +6,41 @@ import time
 import logging
 import datetime
 
+def countloglinestocsv(projectname):
+
+    csvname = projectname+".csv"
+    logname = projectname+".log"
+
+    logfile = open(logname)
+    csvfile = open(csvname,'w')
+
+    logcontents = logfile.readlines()
+
+    lastdate = None
+    datecount = 0
+
+    for l in logcontents:
+
+        # We check the first word
+        firstspacepos = l.find(' ')
+        if firstspacepos != -1:
+
+            newdate = l[:firstspacepos]
+
+            if lastdate != newdate:
+
+                if lastdate:
+                    csvfile.write("%s,%d\n" % (lastdate,datecount))
+
+                datecount = 1
+                lastdate = newdate
+
+            else:
+                datecount += 1
+
+    logfile.close()
+    csvfile.close() 
+
 if __name__ == "__main__":
 
     from optparse import OptionParser
@@ -28,29 +63,33 @@ if __name__ == "__main__":
     print "logline counter"
 
     logging.basicConfig(format='%(asctime)s %(message)s',filename='logline-counter.log',level=logging.DEBUG)
-    
+
     log = logging.getLogger("logline-counter")
     log.setLevel(logging.DEBUG) #set verbosity to show all messages of severity >= DEBUG
     log.info("Program startup")    
 
     purgeafterprocess = options.keep == None
-       
+  
     if not options.filename is None:
         # Processing a single file, implies debugging, so dont delete it
         txtfiles = [options.filename]
-        purgeafterprocess = False
-    else:
-        purgeafterprocess = True
 
-    # purgeafterprocess = False
-    scandirectories = ["/home/user/dir1",
-                       "/home/user/dir2"
-                      ]
+    scandirectories = []
+
+    if len(args) == 1:
+
+        projectname = args[0]
+
+        print os.path.basename(projectname)
+
+        countloglinestocsv(projectname)
+
+        scandirectories.append(projectname)
 
     try:
 
         while (1):
-    
+
             try:
 
                 # Remove files more than two days old
@@ -58,15 +97,15 @@ if __name__ == "__main__":
 
                     twodays_ago = time.time() - 60*60*24*2
                     for scandir in scandirectories:
-                        pdffiles = glob.glob(os.path.join(scandir,"*"))
-                        for pdffile in pdffiles:
-                            fileCreation = os.path.getctime(pdffile)
+                        logfiles = glob.glob(os.path.join(scandir,"*"))
+                        for logfile in logfiles:
+                            fileCreation = os.path.getctime(logfile)
                             if fileCreation < twodays_ago:
                                 log.info("Cleaning up %s as it is more than two days old" % pdffile)
                                 os.remove(pdffile)
 
-                time.sleep(60)             # wait for a second
-    
+                time.sleep(60)             # wait for a minute
+
             except Exception, e:
                 log.exception(e)
 
