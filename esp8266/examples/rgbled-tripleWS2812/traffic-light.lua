122ac9b93ff3ca6f072632fe9ff2864ffc7c3b18
clixx-io <david.lyon@clixx.io>
2016-01-03 06:38:25 +1100

    Initial Checkin

---
+++ esp8266/examples/rgbled-tripleWS2812/traffic-light.lua
@@ -0,0 +1,4 @@
+-- Clixx.io RGB LED Traffic Light
+-- This code is public domain, attribution to david.lyon@clixx.io is appreciated.
+
+ws2812.writergb(3, string.char(0,153,0, 255,102,0, 255,0,0)) -- Displays Green+Orange+Red on a triple WS2812
