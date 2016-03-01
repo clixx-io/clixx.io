12d770e6c67438ff17fb70ecf160f5063598864b
clixx-io <david.lyon@clixx.io>
2016-03-02 06:30:04 +1100

    Improved html reporting

--- esp8266/examples/temp-driven-motor/init.lua
+++ esp8266/examples/temp-driven-motor/init.lua
@@ -10,20 +10,20 @@
 
 require('ds18b20')
 
+print("clixx.io Simple Thermo-Motor Controller")
+
 port = 80
 
--- ESP-01 GPIO Mapping
-gpio_0i, gpio_0o = 4,3
+gpio_0i, gpio_0o = 4,3   -- ESP-01 GPIO Mapping
+orv = 0                  -- PWM Pin Output value
 
-
-orv = 0
-mappingmarkup = ""
+ds18b20.setup(gpio_0i)
 
 pwm.setup(gpio_0o, 500, 512)
 pwm.start(gpio_0o)
 
+-- A lookup table for temperature threshold to PWM value
 tempmappings = {
-             -- A temperature threshold then a PWM value for the output port
              {0,0},
              {10,200},         
              {15,300},
@@ -34,17 +34,22 @@ tempmappings = {
              {40,1023},     
      }            
 
--- Generate a Table showing the current temperature thresholds
-mappingmarkup = "<br/><table border=\"1\" style=\"width:35%\"><caption>Temperature Run Settings</caption>"
-mappingmarkup = mappingmarkup .. "<tr><th>Temperature</th><th>PWM Value 0-1023</th></tr>"
-for k,v in pairs(tempmappings) do
-     mappingmarkup = mappingmarkup .. "<tr>" .. "<td>" .. v[1] .. "</td><td>" .. v[2] .. "</td></tr>"
+function statusReportHtml()
+     -- Generate a Table showing the current temperature thresholds
+     local mappingmarkup
+     
+     mappingmarkup = "<br/><table border=\"1\" style=\"width:35%\"><caption>Temperature Run Settings</caption>"
+     mappingmarkup = mappingmarkup .. "<tr><th>Temperature</th><th>PWM Value 0-1023</th></tr>"
+     for k,v in pairs(tempmappings) do
+          if (v[2] == orv) then
+               mappingmarkup = mappingmarkup .. "<tr bgcolor=\"#1E88E5\">" .. "<td>" .. v[1] .. "</td><td>" .. v[2] .. "</td></tr>"
+          else
+               mappingmarkup = mappingmarkup .. "<tr>" .. "<td>" .. v[1] .. "</td><td>" .. v[2] .. "</td></tr>"
+               end     
+          end
+     mappingmarkup = mappingmarkup .. "</table>"
+     return mappingmarkup 
      end
-mappingmarkup = mappingmarkup .. "</table>"
-
-ds18b20.setup(gpio_0i)
-
-print("clixx.io Temperature Controlled Motor")
 
 srv=net.createServer(net.TCP)
 srv:listen(port,
@@ -52,13 +57,13 @@ srv:listen(port,
           conn:send("HTTP/1.1 200 OK\nContent-Type: text/html\nRefresh: 5\n\n" ..
               "<!DOCTYPE HTML>" ..
               "<html><body>" ..
-              "<h2>Clixx.io Temperature to PWM Controller</h2></br>" ..
+              "<h2>clixx.io Simple Thermo-Motor Controller</h2></br>" ..
               "Temperature : " .. ds18b20.read() .. "<br>" ..
               "Current PWM Run Value : " .. orv .. "<br>" ..
               "Node MAC : " .. wifi.sta.getmac() .. "<br>" ..
               "Node Heap : " .. node.heap() .. "<br>" ..
               "Timer Ticks : " .. tmr.now() .. "<br>" ..
-              mappingmarkup ..
+              statusReportHtml() ..
               "</html></body>")          
           conn:on("sent",function(conn) conn:close() end)
      end
@@ -73,11 +78,12 @@ tmr.alarm(0,1000,1,function()
      ds18b20.setup(gpio_0i)
      return end
      
-  for k,v in pairs(tempmappings) do
-      if t < v[1] then
-          orv = v[2]
-          pwm.setduty(gpio_0o, orv)
-          return end
-  end
+     -- Convert a temperature to a PWM value using the lookup table
+     for k,v in pairs(tempmappings) do
+           if t < v[1] then
+               orv = v[2]
+               pwm.setduty(gpio_0o, orv)
+               return end
+       end
   
 end)
